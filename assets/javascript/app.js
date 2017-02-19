78bbb88841b6bfdf8f68b2938a91b4ec38d4a7a9
Kathleen Pile <Kathleen Pile>
2017-02-18 20:46:11 -0500

    fixed all bugs.  need to add comments and fix coloring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
 var timer = 0;
 var counter= 0;
 var gameOver = false;
-var correctAns;
 var correct=0;
 var wrong=0;
 var unanswered=0;
@@ -21,68 +20,68 @@ var trivia = [
 	    ans: ["Tangled", "The Princess and The Frog", "Flubber", "The Nutty Professor"],
 	    response: [false, false, true, false],
 	    answer: "Flubber was a remake of The Absent Minded Professor",
-	    vid: "assets/images/toy.gif"},
+	    vid: "assets/images/flubber.gif"},
        {
 	    question: "Who was the scrapped cartoon star that came before Mickey Mouse?",
 	    ans: ["Donald Duck", "Horace Horse", "Goofy", "Oswald The Rabbit"],
 	    response: [false, false, false, true],
 	    answer: "Oswald the Rabbit was scrapped as the cartoon star before Mickey Mouse",
-	     vid: "assets/images/toy.gif"},
+	     vid: "assets/images/oswald.gif"},
 	   {
 	    question: "Which Disney villain stars in The Little Mermaid?",
 	    ans: ["King Triton", "Ursula", "Captain Hook", "The Red Queen"],
 	    response: [false, true, false, false],
 	    answer: "Ursula was the villain in The Little Mermaid",
-	     vid: "assets/images/toy.gif"},
+	     vid: "assets/images/ursala.gif"},
 	   {
 	    question: "Which Pixar film takes place in Australia?",
 	    ans: ["Brave", "Cars", "Ratatouille", "Finding Nemo"],
 	    answer: "Finding Nemo takes place in Australia",
 	    response: [false, false, false, true],
-	     vid: "assets/images/toy.gif"},
+	     vid: "assets/images/nemo.gif"},
 	   {
 	    question: "Which Disney movie is a retelling of the story of Hamlet?",
 	    ans: ["The Lion King", "Beauty and The Beast", "Cinderella", "Frozen"],
 	    response: [true, false, false, false],
 	    answer: "The Lion King is based on the story of Hamlet",
-	     vid: "assets/images/toy.gif"},
+	     vid: "assets/images/lion.gif"},
 	   {
 	    question: "Which Disney movie wasn't inspired by a ride at a Disney theme park?",
 	    ans: ["Pirates of The Carribbean", "Tomorrowland", "Cars", "Tower of Terror"],
 	    response: [false, false, true, false],
 	    answer: "Cars is not inspired by a ride at Disney theme parks",
-	     vid: "assets/images/toy.gif"},
+	     vid: "assets/images/cars.gif"},
 	   {
 	    question: "Which Disney movie featured a mix of cartoons and live action actors?",
 	    ans: ["Space Jam", "The Country Bears", "Dumbo", "Mary Poppins"],
 	    response: [false, false, false, true],
 	    answer: "Mary Poppins features a mix of cartoons and live action actors",
-	     vid: "assets/images/toy.gif"},
+	     vid: "assets/images/poppins.gif"},
 	   {
 	    question: "Which Disney movie was later turned into a TV series?",
 	    ans: ["Toy Story", "Aladdin", "A Bug's Life", "Cars"],
 	    response: [ false, true, false, false],
 	    answer: "Aladdin was later turned into a TV series",
-	     vid: "assets/images/toy.gif"},
+	     vid: "assets/images/alladin.gif"},
 	   {
 	    question: "Who sings the featured title song from Frozen",
 	    ans: ["Katy Perry", "Kristen Bell", "Idina Menzel", "Taylor Swift"],
 	    response: [false, false, true, false],
 		answer: "Idina Menzel sings the featured title song from Frozen",
-	     vid: "assets/images/toy.gif"}
+	     vid: "assets/images/frozen.gif"}
 	    ];
 
+
+// Display the question and answers in a button that has already been created
 	 function displayTrivia(){
-		    var j=0
-		    // var currentQues = "quest"+ques;
-		    
+		    var j=0;
 			$("#question").html(trivia[ques].question);
 			$('.triviaBtn').each(function(){	
 				 $(this).removeData("ansr");
 				
-			    if (trivia[ques].response[j]) {
-			    	correctAns = trivia[ques].ans[j];
-			    };
+			    // if (trivia[ques].response[j]) {
+			    // 	correctAns = trivia[ques].ans[j];
+			    // };
 			    $(this).html(trivia[ques].ans[j]);
 				$(this).attr("data-ansr", trivia[ques].response[j]);
 				j++;
@@ -94,6 +93,8 @@ var trivia = [
 
 
 function createBttn(){
+
+		    console.log("createBttn");
 		for (var i = 0; i < 4; i++) {
 		var newBtn = $("<button>");
 		newBtn.attr("class", "triviaBtn btn");
@@ -104,13 +105,18 @@ function createBttn(){
 
 
 function startNewQues(){
+
+    console.log("startNewQues");
 	displayTime();
-	displayTrivia();
 	restartTimer();
+	displayTrivia();
+	
 }
 
 
 function restartTimer () {
+
+		    console.log("restartTimer");
 	 timer = 10;
 	 counter= setInterval(runTimer, 1000);
 }
@@ -118,6 +124,7 @@ function restartTimer () {
 
 function displayTime(){
 	
+		    console.log("displayTime"+ timer);
 	$("#counter").html("Time Remaining: " + timer);
 }
 
@@ -136,8 +143,10 @@ function runTimer(){
 
 
 function displayResults() {
+
+		    console.log("displayResults");
 		$("#question").empty();
-	    $("#counter").remove();
+	    // $("#counter").hide();
 	    var tempDiv  = $("<div>").html("Total Correct Answers: " + correct).attr("class", "results resultsFirst");
 	    $("#answers").append(tempDiv);
 	    tempDiv = $("<div>").html("Total Wrong Answers: " + wrong).attr("class", "results");
@@ -147,12 +156,16 @@ function displayResults() {
 	    begBtn.html("Restart");
 		$("#answers").append(begBtn); 
 
+
 }
 
 
 
 function displayAnswer() {
+
+		    console.log("displayAnswer");
     $(".triviaBtn").hide();
+    console.log("trivia buttons hidden");
 	clearInterval(counter);
     var resultDiv = $("#answers");
     $("#question").html(message);
@@ -164,11 +177,17 @@ function displayAnswer() {
 	    $( ".screen" ).remove();
 		console.log("question # " +ques + " " + trivia.length);
 	    if (ques < trivia.length-1) {
+
+		    console.log("showTriviaBtN");
 	    	$(".triviaBtn").show();
 	        ques++;
 	       startNewQues();
+	       				console.log(" called start new question");
+
 	    } else {
+	    		console.log(" calling display Results");
 	       displayResults();
+	    		console.log(" back from displayig results");
 		}
    }, 1000*3);
 
@@ -189,8 +208,12 @@ $("#answers").append(begBtn);
 
 
 $(".startBtn").on("click", function() {
+
+	console.log("start button pushed");
 	$(".startBtn").detach();
 	$(".results").empty();
+	$("#answers").empty();
+	$("#vidDiv").empty();
 	timer = 0;
 	counter= 0;
 	gameOver = false;
@@ -201,14 +224,17 @@ $(".startBtn").on("click", function() {
     ques = 0;
 	createBttn();
 	startNewQues();
+
 	 $(".triviaBtn").on("click", function(){
 		 	userPick = $(this).data("ansr");
+				console.log("trivia button clicked");
 			if (userPick) {
 				message = "Congratulations!  That is correct.";
 				correct++;
-			} else
+			} else {
 			    message = "That answer is incorrect."
 			    wrong++;
+			   };
 
 			displayAnswer();
 
